3a32941e977176bd2ba27cdf1110a018512b2c6c
0xLadoo <sendev@rage.trade>
2024-01-26 13:47:44 +0530

    chore: updated rpc urls

--- test/legacy.ts
+++ test/legacy.ts
@@ -48,7 +48,7 @@ const sdk = new KwentaSDK({
   networkId: 10,
   provider: rpc[10],
   // provider: new ethers.providers.JsonRpcProvider(
-  //   'https://optimism.blockpi.network/v1/rpc/e9eb838be05076b18bceb9e7efe3797c93bed264',
+  //   'https://optimism.blockpi.network/v1/rpc/eaa4a202e9992d4a8f25d1725b52236e3587cd44',
   //   10
   // )
 })
@@ -728,7 +728,7 @@ async function gmxService() {
   //   ALCHEMY_KEY_OP_MAIN!.toString()
   // );
   // const provider = new ethers.providers.JsonRpcProvider(
-  //   'https://arbitrum.blockpi.network/v1/rpc/6bee49eb5c39a712464e8f39182ff12127c84f48',
+  //   'https://arbitrum.blockpi.network/v1/rpc/3fccabab81b09aeff58df1caaea8f27c70346335',
   //   // "https://rpc.ankr.com/arbitrum",
   //   ARBITRUM
   // )
@@ -1089,13 +1089,13 @@ async function testAutoRouter() {
   const synSupportedMarkets = await ss.supportedMarkets(supportedNetworks[0])
   const synBtcMarket = synSupportedMarkets.find((m) => m.indexOrIdentifier === FuturesMarketKey.sBTCPERP)!
   let synProvider = new ethers.providers.JsonRpcProvider(
-    'https://optimism.blockpi.network/v1/rpc/e9eb838be05076b18bceb9e7efe3797c93bed264',
+    'https://optimism.blockpi.network/v1/rpc/eaa4a202e9992d4a8f25d1725b52236e3587cd44',
     10
   )
 
   // gmx initial setup
   const gmxProvider = new ethers.providers.JsonRpcProvider(
-    'https://arbitrum.blockpi.network/v1/rpc/6bee49eb5c39a712464e8f39182ff12127c84f48',
+    'https://arbitrum.blockpi.network/v1/rpc/3fccabab81b09aeff58df1caaea8f27c70346335',
     // "https://rpc.ankr.com/arbitrum",
     ARBITRUM
   )
