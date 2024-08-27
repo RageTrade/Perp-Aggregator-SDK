d85b14a62008d9992ec560ccdb2ac0753e06fa17
0xLadoo <sendev@rage.trade>
2024-08-27 18:51:01 +0530

    fix: blockpi rpc overload while fetchin synV2 positions

--- sdk/src/services/futures.ts
+++ sdk/src/services/futures.ts
@@ -435,7 +435,11 @@ export default class FuturesService {
     }
 
     // TODO: Combine these two?
-    const positionDetails = (await this.sdk.context.multicallProvider.all(positionCalls)) as PositionDetail[]
+    const positionDetails1 = await this.sdk.context.multicallProvider.all(positionCalls.slice(0, 20)) as PositionDetail[]
+    const positionDetails2 = await this.sdk.context.multicallProvider.all(positionCalls.slice(20, 40)) as PositionDetail[]
+    const positionDetails3 = await this.sdk.context.multicallProvider.all(positionCalls.slice(40, 60)) as PositionDetail[]
+    const positionDetails4 = await this.sdk.context.multicallProvider.all(positionCalls.slice(60, positionCalls.length)) as PositionDetail[]
+    const positionDetails = [...positionDetails1, ...positionDetails2, ...positionDetails3, ...positionDetails4]
     // const canLiquidateState = (await this.sdk.context.multicallProvider.all(
     // 	liquidationCalls
     // )) as boolean[]
